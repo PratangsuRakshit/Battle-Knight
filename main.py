6c340d6769fb152acf51761e24c5f9d7c6522efc
PratangsuRakshit <85653652+PratangsuRakshit@users.noreply.github.com>
2021-08-19 05:46:16 +0530

    Added Gun.obj

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from ursina import *
+rom ursina import *
 from ursina.curve import linear
 from ursina.prefabs.first_person_controller import FirstPersonController
 app = Ursina()
@@ -11,32 +11,34 @@ class Player(FirstPersonController):
         self.gun.position = self.position + self.forward + (1, 0, 0)
 
 class Bullet(Entity):
-    def __init__(self, gun):
+    def __init__(self, gun, player=Entity()):
         super().__init__()
 
         self.parent = gun
         self.model='cube'
         self.scale=.1
         self.world_parent = scene
+        self.rotation_x = 185
         self.animate_position(self.position + (self.forward * 50), curve = linear, duration=1)
 
         destroy(self, delay=1)
         gun.blink(color.red)
 
 class Gun(Entity):
-    def __init__(self):
+    def __init__(self, model='cube', rotation=(0, 0, 0)):
         super().__init__()
-        self.model='cube'
+        self.model=model
         self.texture = 'white_cube'
         self.scale=.5
         self.bullet = Bullet(gun=self)
+        self.rotation = rotation
 
 map1 = load_texture('fps shooter assets/map1.png')
 Entity(model='plane', scale=150, collider='box',texture = map1)
 
-gun = Gun()
-bullet = Bullet(gun=gun)
+gun = Gun(model='fps shooter assets/Gun.obj', rotation=(0, 180, 0))
 player = Player(gun=gun)
+bullet = Bullet(gun=gun, player=player)
 
 def input(key):
     if key == 'left mouse down' and player.gun:
